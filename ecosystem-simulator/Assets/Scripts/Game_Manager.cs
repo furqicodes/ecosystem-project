1570a53ca9f78e648a7eb0ba73a00f07db147bc3
Furqi <tfozkorucuklu@gmail.com>
2019-02-12 01:19:41 +0300

    Added plant population generator

--- ecosystem-simulator/Assets/Scripts/Game_Manager.cs
+++ ecosystem-simulator/Assets/Scripts/Game_Manager.cs
@@ -4,15 +4,29 @@ public class Game_Manager : MonoBehaviour
 {
     public Camera cam;
     public GameObject ground;
+    public GameObject plant;
+    private GameObject plants;
 
     public float worldScale = 1f;
     public float dayLength;
+    public int seasonLengthInDays;
+    [HideInInspector]
     [Range(0, 5)]
-    public float timeMultiplier = 1f;
+    public float timeMultiplier = 1f; //this function doesn't work properly for now
 
+    public bool day = false;
 
     [Range(0, 1)]
     public float currentTimeOfDay = 0;
+    [Range(0, 1)]
+    public float currentSeasonStatus = 0;
+
+    public float populationDensity;
+    public float fertilityRate;
+
+    public int seasonNumber = 0;
+    public int maxPopulation;
+    public int plantCount;
 
     void Awake()
     {
@@ -21,12 +35,72 @@ public class Game_Manager : MonoBehaviour
 
     }
 
+    private void Start()
+    {
+        plants = new GameObject("Plants");
+        GenerateObject(plant, (int)(populationDensity * maxPopulation));
+        plantCount = GameObject.FindGameObjectsWithTag("Plant").Length;
+
+    }
+
     private void Update()
     {
         currentTimeOfDay += (Time.deltaTime / dayLength) * timeMultiplier;
+        day = (currentTimeOfDay > 0.25f && currentTimeOfDay < 0.75f) ? true : false;
 
         if (currentTimeOfDay >= 1)
             currentTimeOfDay = 0;
 
+        currentSeasonStatus += (Time.deltaTime / (dayLength * seasonLengthInDays)) * timeMultiplier;
+
+        if (currentSeasonStatus >= 1)
+        {
+            currentSeasonStatus = 0;
+            seasonNumber++;         //World-like seasons will be added.
+            Season();
+        }
+
+
     }
+
+
+    void Season()
+    {
+        plantCount = GameObject.FindGameObjectsWithTag("Plant").Length;
+        populationDensity = populationDensity * (1 - populationDensity) * fertilityRate;
+        int k = (int)(populationDensity * maxPopulation);
+        if (plantCount < k)
+            GenerateObject(plant, k - plantCount);
+
+        else if (k < plantCount)
+            DestroyObject("Plant", plantCount - k);
+
+    }
+
+    public Vector3 PickPosition()
+    {
+        return new Vector3(Random.Range(-worldScale * 5f + 0.5f, worldScale * 5f - 0.5f), 0.15f, Random.Range(-worldScale * 5f + 0.5f, worldScale * 5f - 0.5f)); //-0.5f is for avoiding animals from falling out of world
+    }
+
+    void GenerateObject(GameObject obj, int repetation)
+    {
+        for (int i = 0; i < repetation; i++)
+        {
+            Instantiate(obj, PickPosition(), Quaternion.identity);
+        }
+
+        foreach (var item in GameObject.FindGameObjectsWithTag("Plant"))
+        {
+            item.transform.parent = plants.transform;
+        }
+    }
+
+    void DestroyObject(string obj, int repetation)
+    {
+        for (int i = 0; i < repetation; i++)
+        {
+            Destroy(GameObject.FindGameObjectWithTag(obj));
+        }
+    }
+
 }
--- ecosystem-simulator/Assets/Scripts/PlantCode.cs
+++ ecosystem-simulator/Assets/Scripts/PlantCode.cs
@@ -2,25 +2,26 @@
 
 public class PlantCode : MonoBehaviour
 {
-    private float timeOfDay;
+    private float timeMultiplier;
     private float dayLength;
     public float t;
-    public bool day = false;
+    private bool day;
 
     private void Start()
     {
         dayLength = GameObject.Find("gameManager").GetComponent<Game_Manager>().dayLength;
+        timeMultiplier = GameObject.Find("gameManager").GetComponent<Game_Manager>().timeMultiplier;
     }
 
     private void Update()
     {
-        timeOfDay = GameObject.Find("gameManager").GetComponent<Game_Manager>().currentTimeOfDay;
-        day = (timeOfDay > 0.25f && timeOfDay < 0.75f) ? true : false;
+        //timeOfDay = GameObject.Find("gameManager").GetComponent<Game_Manager>().currentTimeOfDay;
+        day = GameObject.Find("gameManager").GetComponent<Game_Manager>().day;
 
         if (day)
         {
             transform.localScale = new Vector3(Mathf.Lerp(0.5f, 1f, t), 0.3f, Mathf.Lerp(0.5f, 1f, t));
-            t += 2 / (dayLength) * Time.deltaTime;
+            t += (2 * Time.deltaTime) / dayLength;
         }
 
     }
